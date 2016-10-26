97d4abece84000d7d355bb2f42f4a0ac8eb9dca2
shenzhenbiao <shenzhenbiao@126.com>
2016-10-26 19:56:24 +0800

    test

--- GuavaPro/src/main/java/com/hrricane/guava/pojo/Foo.java
+++ GuavaPro/src/main/java/com/hrricane/guava/pojo/Foo.java
@@ -1,13 +1,13 @@
 package com.hrricane.guava.pojo;
 
-import com.sun.istack.internal.Nullable;
+//import com.sun.istack.internal.Nullable;
 
 /**
  * Created by zhenbiao.shen on 2016/7/7.
  */
 public class Foo {
 
-    @Nullable
+//    @Nullable
     public
     String sortedBy;
 
